100864fd5681412c7bb6e4949f68fd2b1d63fe80
Jonathan Prieto <jonathan.prieto@me.com>
2019-10-23 17:16:10 -0500

    Upload Files

---
+++ cmake-build-debug/CMakeFiles/roman-numerals-11616-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/roman-numerals-11616-jonathanprieto99.dir/main.cpp.o"
+  "roman-numerals-11616-jonathanprieto99"
+  "roman-numerals-11616-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/roman-numerals-11616-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
